6418935baf10661f608bedb78f7f3ae27ba3b6f0
KennethJackson <kenneth@whoiskenjackson.com>
2015-07-12 03:02:52 -0700

    updating functionality to check for user data

--- website/js/app/dashboard/main.js
+++ website/js/app/dashboard/main.js
@@ -1,8 +1,47 @@
-define(function (require) {
+define(function(require) {
 
-    var setup = require('./setup/setup');
-    var userSettings = {};
+	var setup = require('setup/setup');
+    var userInfoURL = "js/settings/user.json";
+    var userData = localStorage.getItem("userData");
 
-    setup.init(userSettings);
+	var initDashboard = function(data) {
+		setup.init(data);
+	}
+
+	var getData = function (url) {
+		$.ajax({
+			dataType: "json",
+			type: 'GET',
+			url: url,
+			success: function(model){
+				var data = JSON.stringify(model);
+
+				localStorage.setItem("userData", data);
+				initDashboard(model);
+			},
+			error: function(){
+				initSetup();
+			}
+		});
+	}
+
+	var initSetup = function() {
+		var model = {
+			"name": null,
+			"team": null,
+			"news": [],
+			"players": []
+		}
+		var data = JSON.stringify(model);
+
+		localStorage.setItem("userData", data);
+		setup.init(model);
+	}
+
+	if(userData) {
+		initDashboard(userData);
+	} else {
+		getData(userInfoURL);
+	}
 
 });
--- website/js/app/dashboard/setup/setup.js
+++ website/js/app/dashboard/setup/setup.js
@@ -1,22 +1,14 @@
 define(function () {
 
 	return {
-		init: function (model) {
+		init: function(model) {
 
-			/*
-			 * Setup Options
-			 *
-			 */
 			this.opt = {
-				scope: $(".screen-welcome"),
+				scope: $("#main"),
 				templates: Handlebars.templates,
 				model: model
 			}
 
-			/*
-			 * Shorten Reference to Scope
-			 *
-			 */
 			this.$el = this.opt.scope;
 			this.model = this.opt.model;
 
@@ -36,7 +28,6 @@ define(function () {
 			// When you click the next button
 			this.$el.find(".btn-next").on("click", function(e) {
 				e.preventDefault();
-
 				$this = $(this);
 				self.animateToNextSlide($this);
 			});
@@ -44,32 +35,44 @@ define(function () {
 			// When you click the get started button
 			this.$el.find(".btn-getStarted").on("click", function(e) {
 				e.preventDefault();
-
-				$this = $(this);
 				self.storeUserInformation();
 				self.animateToHomeScreen();
 			});
 		},
 
 		animateToNextSlide: function(elm) {
-			var $parent = elm.parent();
-			var $pagination = this.$el.find(".pagination");
-			var $currentPagItem = $pagination.find(".active");
+			var $parent = elm.parent(); // Find the Parent
+			var $pagination = this.$el.find(".pagination"); // Find the Pagination
+			var $currentPagItem = $pagination.find(".active"); // Find the Active Item in the Pagination
 
-			$parent.addClass("finish");
-			$parent.next().addClass("active");
+			if(this.$el.find("#name").val() != "") {
+				// Change Classes to Animate Screens
+				$parent.addClass("finish");
+				$parent.next().addClass("active");
 
-			$currentPagItem.removeClass("active");
-			$currentPagItem.next().addClass("active");
+				// Change Pagination Classes
+				$currentPagItem.removeClass("active");
+				$currentPagItem.next().addClass("active");
+			}
 		},
 
 		storeUserInformation: function() {
-			this.model.name = this.$el.find("#name").val();
-			this.model.team = this.$el.find("#team").val();
+			var $elm = this.$el.find(".screen-welcome");
+			this.model.name = $elm.find("#name").val();
+			this.model.team = $elm.find("#team").val();
+
+			var data = JSON.stringify(this.model);
+			localStorage.setItem("userData", data);
+
+			setTimeout(function(){
+				$elm.remove();
+			}, 1000);
 		},
 
 		animateToHomeScreen: function() {
-			this.$el.addClass("animate-out");
+			if(this.$el.find("#team").val() != "") {
+				this.$el.find(".screen-welcome").addClass("animate-out");
+			}
 		}
 	};
 });
