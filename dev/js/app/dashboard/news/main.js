d38dfccf51e21ac52d6cf65bb4a0e87a33bfdfa3
Kenneth Jackson <kenneth@whoiskenjackson.com>
2015-07-20 21:27:46 +0000

    Updating news page layout

--- dev/js/app/dashboard/news/main.js
+++ dev/js/app/dashboard/news/main.js
@@ -1,21 +1,30 @@
 define(function (require) {
-
-	var userData = localStorage.getItem("userData");
-	var model = JSON.parse(userData);
-
-	this.opt = {
-		scope: $("#main"),
-		templates: Handlebars.templates,
-		model: model
+	
+	return {
+	    
+	    init: function(model) {
+	        
+	        model = JSON.parse(model);
+	        
+	        this.opt = {
+        		scope: $("#main"),
+        		templates: Handlebars.templates,
+        		model: model
+        	}
+        	
+        	this.$el = this.opt.scope;
+	        this.model = this.opt.model;
+	        
+	        this.renderTemplate();
+        	
+	    },
+	    
+	    renderTemplate: function() {
+	        
+	        this.$el.html(this.opt.templates["news"](this.model));
+	        
+	    }
+	    
 	}
-
-	this.$el = this.opt.scope;
-	this.model = this.opt.model;
-
-	var renderTemplate = function() {
-		this.$el.html(this.opt.templates["news"](this.model));
-	}
-
-	renderTemplate();
 
 });
