67d7a3912d33dccca974f35da44ccab7904060e5
KennethJackson <kenneth@whoiskenjackson.com>
2015-07-20 20:02:57 -0700

    updating preferences functionality

--- dev/js/app/dashboard/preferences/main.js
+++ dev/js/app/dashboard/preferences/main.js
@@ -1,51 +1,67 @@
 define(function (require) {
-	
+
 	return {
-	    
+
 	    init: function(model) {
-	        
+
 	        model = JSON.parse(model);
-	        
+
 	        this.opt = {
         		scope: $("#main"),
         		templates: Handlebars.templates,
         		model: model
         	}
-        	
+
         	this.$el = this.opt.scope;
 	        this.model = this.opt.model;
-	        
+
 	        this.renderTemplate();
-        	
+
 	    },
-	    
+
 	    renderTemplate: function() {
-	        
+
 	        this.$el.html(this.opt.templates["preferences"](this.model));
 	        this.bindEvents();
-	        
+
 	    },
-	    
+
 	    bindEvents: function() {
-	        
+
 	        var self = this;
-	        
+
 	        this.$el.find(".btn-save").on("click", function(e) {
 	            e.preventDefault();
 	            self.savePreferences();
 	        });
+
 	    },
-	    
+
 	    savePreferences: function() {
+
 	        var self = this;
-	        console.log(this.model);
+
 	        this.$el.find("[name='news']").each(function() {
+
+	           var network = $(this).val();
+
 	           if($(this).is(":checked")) {
-	               console.log(this);
+
+	               _.set(self.model.news, network, true);
+
+	           } else {
+
+	               _.set(self.model.news, network, false);
+
 	           }
+
 	        });
+
+	        var data = JSON.stringify(this.model);
+	        localStorage.setItem("userData", data);
+
 	    }
-	    
+
 	}
 
 });
